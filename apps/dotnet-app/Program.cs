1c950c0523c921ca09e2f75dffc4688ab6fe9671
Jonathan Cammisuli <jon@cammisuli.ca>
2020-05-13 11:29:57 -0400

    add dotnet package

---
+++ apps/dotnet-app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using dotnet_lib;
+
+namespace dotnet_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Class1().saySomething();
+        }
+    }
+}
