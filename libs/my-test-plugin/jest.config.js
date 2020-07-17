01ef7f86fcd287ee1185ba091567bc30be75ce9d
Jonathan Cammisuli <jon@cammisuli.ca>
2020-07-17 11:12:32 -0400

    update to v10b7 (#6)

--- libs/my-test-plugin/jest.config.js
+++ libs/my-test-plugin/jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/my-test-plugin'
+  coverageDirectory: '../../coverage/libs/my-test-plugin',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } }
 };
